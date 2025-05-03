9fea0266183566fa200a2db0f3d612031c9f3a59
petrosAth <develop@athanasoulis.me>
2025-05-03 15:28:14 +0300

    Add models draft
    Commit migration

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,33 @@
+from enum import unique
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class Listings(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    starting_price = models.FloatField(max_length=6)
+    photo_url = models.URLField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="auctions")
+    active = models.BooleanField()
+    winner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="purchases")
+
+
+class Bids(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    listing = models.ForeignKey(Listings, on_delete=models.CASCADE, related_name="bids")
+    amount = models.FloatField(max_length=6)
+
+
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    listing = models.ForeignKey(
+        Listings, on_delete=models.CASCADE, related_name="comments"
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
