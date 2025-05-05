d01398ff6f83196f336b82d85187657f0dd0fe55
petrosAth <develop@athanasoulis.me>
2025-05-05 22:20:22 +0300

    Enable admin panel

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import User, Listing, Bid, Category, Comment
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "email")
+
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "description", "starting_price")
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "description")
+
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Category, CategoryAdmin)
